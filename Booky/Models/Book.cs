d5e9bc920542cd079764777af426fa76ee0601a7
TaniaTrue <tatiana.trukhachova@gmail.com>
2018-11-30 13:20:23 +0200

    model classes

---
+++ Booky/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Booky.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        //[Required]
+        //[StringLength(255)]
+        public string Name { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        public byte NumberInStock { get; set; }
+
+        public byte NumberAvailable { get; set; }
+    }
+}
---
+++ Booky/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Booky.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        //[Required(ErrorMessage = "Please enter customer's name")]
+        //[StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsletter { get; set; }
+
+
+        public DateTime? BirthDate { get; set; }
+    }
+}
