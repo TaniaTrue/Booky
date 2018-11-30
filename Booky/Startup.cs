19d304381bff6862f5128e1527575a072ab21c2f
TaniaTrue <tatiana.trukhachova@gmail.com>
2018-11-30 12:42:38 +0200

    Add project files.

---
+++ Booky/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Booky.Startup))]
+namespace Booky
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
